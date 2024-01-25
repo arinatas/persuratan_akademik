8c9e52d2d1c3a3fc3ab1c211de1a348b177ec1be
arinata098 <arinata098@gmail.com>
2024-01-25 12:27:48 +0800

    remove side bar menu on mobile phone on panduan

--- resources/views/user/dashboard/panduan.blade.php
+++ resources/views/user/dashboard/panduan.blade.php
@@ -48,26 +48,91 @@ License: For each use you must have a valid license purchased only from above li
 					<!--begin::Header-->
 					<div id="kt_header" style="" class="header align-items-stretch">
 						<!--begin::Container-->
-						<div class="container-xxl d-flex align-items-stretch justify-content-between">
-							<!--begin::Aside mobile toggle-->
-							<!--end::Aside mobile toggle-->
-							<!--begin::Logo-->
-							<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0 me-lg-15">
-								<a href="../../demo1/dist/index.html">
-									<img alt="Logo" src="assets/media/logos/logo-1.svg" class="h-20px h-lg-30px" />
-								</a>
+						<div class="container-fluid d-flex align-items-stretch justify-content-between">
+						  <!--begin::Mobile logo-->
+						  <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
+							<a href="/userDashboard" class="d-lg-none">
+							  <img alt="Logo" src="assets/media/logos/smallprimakara.png" class="h-30px" />
+							</a>
+						  </div>
+						  <!--end::Mobile logo-->
+						  <!--begin::Wrapper-->
+						  <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
+							<!--begin::Navbar-->
+							<div class="d-flex align-items-stretch" id="kt_header_nav">
+							  <!--begin::Menu wrapper-->
+							  <div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
+								<!--begin::Menu-->
+								<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
+								  <h1 class="mt-6">{{ $title }}</h1>
+								</div>
+								<!--end::Menu-->
+							  </div>
+							  <!--end::Menu wrapper-->
 							</div>
-							<!--end::Logo-->
-							<!--begin::Wrapper-->
-							<div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
-								<!--begin::Navbar-->
-				                    @include('partials.navbar')
-								<!--end::Navbar-->
+							<!--end::Navbar-->
+							<!--begin::Topbar-->
+							<div class="d-flex align-items-stretch flex-shrink-0">
+							  <!--begin::Toolbar wrapper-->
+							  <div class="d-flex align-items-stretch flex-shrink-0">
+								<!--begin::User-->
+								@if (auth()->check())
+								  <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
+									<!--begin::Menu wrapper-->
+									<div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+									  <img src="assets/media/avatars/blank.png" alt="user" />
+									</div>
+									<!--begin::Menu-->
+									<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
+									  <!--begin::Menu item-->
+									  <div class="menu-item px-3">
+										<div class="menu-content d-flex align-items-center px-3">
+										  <!--begin::Avatar-->
+										  <div class="symbol symbol-50px me-5">
+											<img alt="Logo" src="assets/media/avatars/blank.png" />
+										  </div>
+										  <!--end::Avatar-->
+										  <!--begin::Username-->
+										  <div class="d-flex flex-column">
+											<div class="fw-bolder d-flex align-items-center fs-5">{{ auth()->user()->username }}</div>
+										  </div>
+										  <!--end::Username-->
+										</div>
+									  </div>
+									  <!--end::Menu item-->
+									  <!--begin::Menu separator-->
+									  <div class="separator my-2"></div>
+									  <!--end::Menu separator-->
+									  <!--begin::Menu item-->
+									  <div class="menu-item px-5">
+										<form action="/logout" method="POST">
+										  @csrf
+										
+										<button type="submit" class="dropdown-item menu-link px-5">Sign Out</button>
+					  
+										</form>
+									  </div>
+									  <!--end::Menu item-->
+									</div>
+									<!--end::Menu-->
+									<!--end::Menu wrapper-->
+								  </div>
+								@else
+								  <div class="">
+									<a href="{{ url('login') }}" type="submit" class="btn btn-sm btn-primary mt-4">Login</a>
+								  </div>
+								@endif
+								
+								<!--end::User -->
+							  </div>
+							  <!--end::Toolbar wrapper-->
 							</div>
-							<!--end::Wrapper-->
+							<!--end::Topbar-->
+						  </div>
+						  <!--end::Wrapper-->
 						</div>
 						<!--end::Container-->
-					</div>
+					  </div>
 					<!--end::Header-->
 					<!--begin::Content-->
 					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
